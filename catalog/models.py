2b53a21e634bbbc73c66ae5dce8ce673a2082636
Daniel Franco <danielfranco2011@gmail.com>
2025-08-08 00:45:46 -0700

    Required models attribute layout

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Book - title, author, isbn, page count, location pk, status (available, checked-out, wishlist), cover image, version #, pub date
+
+# User - email & password (email required to send notifs), phone optional, location pk
+
+# Location - PK, location name (in case location name)
